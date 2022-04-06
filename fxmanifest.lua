33767c8ffcac12c586434c9f353cb17f70459c68
CsokiHUN <feketetibi71@gmail.com>
2022-04-06 13:42:33 +0200

    init resource files

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version("cerulean")
+game("gta5")
+author("Csoki")
+
+shared_script("@es_extended/imports.lua")
+shared_script("shared.lua")
+
+server_script("server.lua")
+client_script("client.lua")
