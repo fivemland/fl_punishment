5044a6ff2e95f7a424053a52a3de2edfcc949af0
Csoki <feketetibi71@gmail.com>
2022-11-14 22:08:20 +0100

    fix ui locales

--- ui/src/components/JailInfo.svelte
+++ ui/src/components/JailInfo.svelte
@@ -17,10 +17,10 @@
   <main id="infopanel" class="bg-gray-700 p-2 rounded-xl select-none">
     <div class="text-center font-bold mb-2">{$_('jail')}</div>
 
-    <InfoBar title={$_('time_left')} value={`${(jail.all || 0) - (jail.count || 0)} minutes`} />
-    <InfoBar title={$_('all_time')} value={`${jail.all} minutes`} />
+    <InfoBar title={$_('time_left')} value={`${(jail.all || 0) - (jail.count || 0)} ${$_('minutes')}`} />
+    <InfoBar title={$_('all_time')} value={`${jail.all} ${$_('minutes')}`} />
     <InfoBar title={$_('reason')} value={jail.reason} />
-    <InfoBar title={$_('admin')} value={jail.admin ? jail.admin.name : 'Unknown'} />
+    <InfoBar title={$_('admin')} value={jail.admin ? jail.admin.name : $_('unknown')} />
     <InfoBar title={$_('start_date')} value={formatedDate} />
   </main>
 {/if}
