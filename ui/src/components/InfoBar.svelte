0bb35d9beda76793e39d32a4cedd107e95bfaf1b
Csoki <feketetibi71@gmail.com>
2022-11-14 18:56:02 +0100

    fix infobar styles

--- ui/src/components/InfoBar.svelte
+++ ui/src/components/InfoBar.svelte
@@ -3,9 +3,9 @@
   export let value = '';
 </script>
 
-<div class="grid grid-flow-col gap-1 px-2 text-center items-center bg-slate-800 border-b border-gray-900">
-  <div class="w-20">{title}</div>
-  <div class="w-40">
+<div class="grid grid-cols-2 gap-1 px-2 text-center items-center bg-slate-800 border-b border-gray-900">
+  <div class="max-w-[10rem]">{title}</div>
+  <div class="max-w-xs">
     {value}
   </div>
 </div>
