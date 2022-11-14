83d8157c1ae58df4a601daf930b887a95baa2a7f
Csoki <feketetibi71@gmail.com>
2022-11-14 21:08:51 +0100

    fix ui locale loading

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -3,7 +3,7 @@
   import AdminPanel from './components/AdminPanel.svelte';
   import JailInfo from './components/JailInfo.svelte';
   import { onMount } from 'svelte';
-  import { locale } from 'svelte-i18n';
+  import { isLoading, locale } from 'svelte-i18n';
 
   onMount(async () => {
     const resp = await fetch(`https://${GetParentResourceName()}/getLocale`);
@@ -14,7 +14,9 @@
 </script>
 
 <main>
-  <ComservInfo />
-  <AdminPanel />
-  <JailInfo />
+  {#if !$isLoading}
+    <ComservInfo />
+    <AdminPanel />
+    <JailInfo />
+  {/if}
 </main>
