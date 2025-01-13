cb2b1930276c6c4388b30e7faddbc116c4dc3305
Senna <62171904+Poseidon281@users.noreply.github.com>
2025-01-13 12:42:23 +0100

    Create nl.lua
    Added a Dutch translation to the script

---
+++ locales/nl.lua
@@ -0,0 +1,76 @@
+Locales.nl = {
+  cannot_leave = "Je kunt deze plek niet verlaten",
+  work_help = "Druk op ~INPUT_CONTEXT~ om te beginnen met werken",
+  users_not_found = "Gebruikers niet gevonden!",
+  user_not_found = "Gebruiker niet gevonden!",
+  loading_error = "Fout bij het laden van gebruiker!",
+  invalid_syntax = "Ongeldige syntax!",
+  player_in_jail = "Speler zit al in de admin gevangenis!",
+  player_in_comserv = "Speler zit al in een taakstraf!",
+  work_allocated = "Werk toegewezen aan de speler. Reden: %s",
+  assigned_you = "%s heeft je een taakstraf van %s toegewezen.",
+  reason = "Reden: %s",
+  comserv_log = [[
+    **%s** heeft **%s** *(%s)* toegewezen aan taakstraf
+    Aantal: **%s**
+    Reden: **%s**
+  ]],
+  player_not_in_comserv = "Speler zit niet in een taakstraf.",
+  removed_from_comserv = "Je hebt de speler verwijderd uit de taakstraf.",
+  removed_you_comserv = "%s heeft je verwijderd uit de taakstraf",
+  removecomserv_log = [[
+    **%s** heeft **%s** *(%s)* verwijderd uit taakstraf
+  ]],
+  ban_log = [[
+    **%s** heeft **%s** *(%s)* verbannen
+    Identifier: **%s**
+    Dagen: **%s**
+    Reden: **%s**
+  ]],
+  infinity = "Oneindig",
+  ban_message = [[
+    Je bent verbannen van de server
+    Admin: %s
+    Dagen: %s
+    Reden: %s
+  ]],
+  invalid_days = "Ongeldige waarde voor dagen!",
+  no_reason = "Geen reden opgegeven",
+  you_banned = "Je hebt de speler verbannen, %s",
+  days = "Dagen: %s",
+  reason = "Reden: %s",
+  checking_ban = "Controleer banstatus...",
+  you_banned_from_server = [[
+    \nJe bent verbannen van de server
+    Admin: %s
+    Dagen: %s
+    Einddatum: %s
+    Reden: %s
+    Identifier: %s
+  ]],
+  ban_clear = "Ban opgeheven.",
+  player_not_banned = "Speler is niet verbannen!",
+  player_unbanned = "Speler niet langer verbannen. Naam: %s",
+  unban_log = [[
+    **%s** heeft de ban van **%s** opgeheven
+    Identifier: **%s**
+  ]],
+  time_not_number = "Tijd is geen getal!",
+  jail_allocated = "Gevangenisstraf toegewezen aan de speler. Reden: %s",
+  jail_assigned = "%s heeft je %s minuten admin gevangenis gegeven.",
+  adminjail_log = [[
+    **%s** heeft **%s** *(%s)* naar de admin gevangenis gestuurd
+    Tijd: **%s** minuten
+    Reden: **%s**
+  ]],
+  player_not_in_jail = "Speler zit niet in de admin gevangenis!",
+  you_remove_jail = "Je hebt de speler uit de admin gevangenis gehaald.",
+  removed_from_jail = "%s heeft je uit de admin gevangenis gehaald",
+  unjail_log = [[
+    **%s** heeft **%s** *(%s)* uit de admin gevangenis gehaald
+  ]],
+  count_invalid = "Aantal is ongeldig!",
+  adminjail_over = "Admin gevangenis voorbij.",
+  not_admin = "Je bent geen admin!",
+  removed_punishment = "Je hebt de straf verwijderd."
+}
